d98b69365b736041a745cea1aec36922667a5093
Shakil Ahmed <shakil.devcraft@gmail.com>
2025-02-28 19:28:50 +0600

    sum using for loop

--- range.js
+++ range.js
@@ -1,6 +1,6 @@
 let num = 1;
 
-while(num <= 10){
+while(num <= 100){
     console.log(num);
     num++;
 }
---
+++ sum.js
@@ -0,0 +1,8 @@
+let num = 1;
+let sum = 0;
+
+while(num <= 10){
+    sum += num;
+    console.log('sum:', sum);
+    num++;
+}
