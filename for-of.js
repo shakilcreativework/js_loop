3d871db1fc4b20b029990f284f7926de436afc56
Shakil Ahmed <shakil.devcraft@gmail.com>
2025-02-28 17:48:50 +0600

    Js loop

---
+++ for-of.js
@@ -0,0 +1,13 @@
+const numbers = [35, 34, 53, 643];
+
+// console.log(numbers);
+
+// for(const num of numbers){
+//     console.log(num);
+// }
+
+const fruits = ['orange', 'apple', 'banana', 'jackfruit', 'watermelon'];
+
+for(const fruit of fruits){
+    console.log(fruit);
+}
---
+++ recap.js
@@ -0,0 +1,19 @@
+// const : value will not change
+// let : value can change
+// var : we should not use it because it is old verson and use otherways let
+
+// variable
+const amount = 200;
+
+// condition
+if(amount > 100){
+
+}else{
+
+}
+
+
+// array
+const numbers = [35, 34, 53, 643];
+
+
