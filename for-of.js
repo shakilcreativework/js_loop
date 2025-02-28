9d89593d532da73b8812725bf8b279b8cda00656
Shakil Ahmed <shakil.devcraft@gmail.com>
2025-02-28 17:55:17 +0600

    while loop

--- for-of.js
+++ for-of.js
@@ -10,4 +10,5 @@ const fruits = ['orange', 'apple', 'banana', 'jackfruit', 'watermelon'];
 
 for(const fruit of fruits){
     console.log(fruit);
+    // console.log('I want to eat');
 }
---
+++ while.js
@@ -0,0 +1,6 @@
+const number = 7;
+
+if(number < 1){
+    console.log('choto ekta number paisi');
+}
+console.log('something new is cooking');
