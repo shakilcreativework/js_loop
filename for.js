ede79b582f155777d576d184f452fb0289ae30dd
Shakil Ahmed <shakil.devcraft@gmail.com>
2025-02-28 19:55:54 +0600

    for loop even and odd operation

--- for.js
+++ for.js
@@ -6,6 +6,30 @@
 
 
 // for loop
-for(let num = 1; num <= 5; num++){
-    console.log(num);
+// for(let num = 1; num <= 5; num++){
+//     console.log(num);
+// }
+
+
+// for(let i = 1; i <= 20; i++){
+//     if(i % 2 === 0){
+//         console.log(i);
+//     }
+// }
+
+
+// for(let i = 1; i <= 20; i++){
+//     if(i % 2 === 1){
+//         console.log(i);
+//     }
+// }
+
+
+// for(let i = 0; i <= 20; i+=2){ // even number get
+//     console.log(i);
+// }
+
+
+for(let i = 1; i <= 20; i+=2){ // odd number get
+    console.log(i);
 }
