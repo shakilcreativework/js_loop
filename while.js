a7c523f218b0061437ffa824ecdd38f4fde4124e
Shakil Ahmed <shakil.devcraft@gmail.com>
2025-02-28 18:09:57 +0600

    while loop testing

--- while.js
+++ while.js
@@ -1,6 +1,22 @@
 const number = 7;
 
-if(number < 1){
-    console.log('choto ekta number paisi');
+// if(number < 1){
+//     console.log('choto ekta number paisi');
+// }
+// console.log('something new is cooking');
+
+
+
+// -------------While----Loop----------------
+let num = 0; // loop variable
+while(num < 5){
+    // num = num + 1;
+    // num += 1;
+    num ++;
+    console.log('looping', num);
 }
-console.log('something new is cooking');
+
+
+for(num = 0; num < 5; num++){
+    console.log('loooping', num);
+}
