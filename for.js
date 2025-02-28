e71eb5a43ef45127ffb1be3db18febe34d588abc
Shakil Ahmed <shakil.devcraft@gmail.com>
2025-02-28 19:47:24 +0600

    for loop structure

--- for.js
+++ for.js
@@ -1,5 +1,11 @@
-let num = 0; // loop variable
-while(num < 5){ // loop condition
-    console.log(num); // loop statement
-    num++;
+// let num = 0; // loop variable
+// while(num < 5){ // loop condition
+//     console.log(num); // loop statement
+//     num++;
+// }
+
+
+// for loop
+for(let num = 1; num <= 5; num++){
+    console.log(num);
 }
