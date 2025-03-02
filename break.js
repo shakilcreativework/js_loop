834d417a01cd17e8f4cbe8dc673d3318df0d5a25
Shakil Ahmed <shakil.devcraft@gmail.com>
2025-03-02 10:26:18 +0600

    again try break

---
+++ break.js
@@ -0,0 +1,41 @@
+
+// for(let i = 0; i < 15; i++){
+//     console.log(i);
+// }
+
+// for(let i = 0; i < 15; i++){
+//     if(i === 10){
+//         break;
+//     }
+    
+//     console.log(i);
+// }
+
+// for(let i = 0; i < 15; i++){
+//     if(i >= 5){
+//         break;
+//     }
+
+//     console.log(i);
+// }
+
+// console.log('life after break up');
+
+
+let n = 54;
+
+// while(n > 25){
+//     console.log(n);
+//     if(n < 50){
+//         break;
+//     }
+//     n--;
+// }
+
+while(n > 25){
+    if(n < 50){
+        break;
+    }
+    console.log(n);
+    n--;
+}
