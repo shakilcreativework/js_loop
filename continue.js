6d2ab075231ba266080f9055bb326f59a773e0b4
Shakil Ahmed <shakil.devcraft@gmail.com>
2025-03-02 14:43:54 +0600

    do while loop

---
+++ continue.js
@@ -0,0 +1,29 @@
+// continue => skip this iteration
+// break => i am done with this loop. loop end.
+
+// for(let i = 1; i < 10; i++){
+//     if(i % 2 === 1){
+//         continue;
+//     }
+//     console.log(i);
+// }
+
+
+// for(let i = 1; i < 10; i++){
+//     if(i % 2 === 0){
+//         continue;
+//     }
+//     console.log(i);
+// }
+
+
+let n = 0;
+while(n < 50){
+    n++;
+    if(n % 5 !== 0){
+        continue;
+    }
+    console.log(n);
+}
+
+console.log('end here', n);
